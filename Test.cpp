26a569e405ba8faea445ad22e240ceb4482359d9
Aman Atar <amanattar309@gmail.com>
2022-11-03 10:45:06 +0530

    Substraction fuction added

--- Test.cpp
+++ Test.cpp
@@ -1,10 +1,24 @@
 #include<iostream>
 using namespace std;
+void Sub(int x,int y)
+{
+    cout<<"Substraction of two numbers is :- "<<x-y;
+}
 
 int main()
 {
-    int a,b;
+    int a,b,choice;
     cout<<"Enter two numbers:-";
     cin>>a>>b;
     cout<<"\nAddition of two numbers is:- "<<a+b;
+    cout<<"\nDo you want to subtract as well ?";
+    cin>>choice;
+    if(choice==1)
+    {
+        exit;
+    }
+    else
+    {
+        Sub(a,b);
+    }
 }
